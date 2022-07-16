ca957c52947374ae31a96b04af29ee30d06cbbde
somgr <deniska9767@gmail.com>
2022-07-16 15:45:44 +0500

    add func

--- main.py
+++ main.py
@@ -2,26 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 from time import sleep
 
-list_card_url = []
 headers = {"User-Agent":
            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}
 
-for count in range(1, 8):
-    sleep(1)
-    url = f'https://scrapingclub.com/exercise/list_basic/?page={count}'
+def get_url():
+    for count in range(1, 8):
 
-    response = requests.get(url, headers=headers)
+        url = f'https://scrapingclub.com/exercise/list_basic/?page={count}'
 
-    soup = BeautifulSoup(response.text, 'lxml')
+        response = requests.get(url, headers=headers)
 
-    data = soup.find_all('div', class_='col-lg-4 col-md-6 mb-4')
+        soup = BeautifulSoup(response.text, 'lxml')
 
-    for i in data:
-        card_url = "https://scrapingclub.com" + i.find('a').get('href')
-        list_card_url.append(card_url)
+        data = soup.find_all('div', class_='col-lg-4 col-md-6 mb-4')
 
-for card_url in list_card_url:
+        for i in data:
+            card_url = "https://scrapingclub.com" + i.find('a').get('href')
+            yield card_url
+
+for card_url in get_url():
     response = requests.get(card_url, headers=headers)
+    sleep(3)
     soup = BeautifulSoup(response.text, 'lxml')
 
     data = soup.find('div', class_='card mt-4 my-4')
