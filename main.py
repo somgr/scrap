93c53f1483e32eb0bda11917fdff974ae5a7f4a1
somgr <deniska9767@gmail.com>
2022-07-16 12:44:15 +0500

    get name, price, image url

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+
+url = 'https://scrapingclub.com/exercise/list_basic/?page=1'
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'lxml')
+
+data = soup.find('div', class_='col-lg-4 col-md-6 mb-4')
+
+name = data.find('h4', class_='card-title').text.replace('\n', '')
+price = data.find('h5').text
+url_img = 'https://scrapingclub.com' + data.find('img', class_='card-img-top img-fluid').get('src')
