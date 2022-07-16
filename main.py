3c54ca91087a76ce1a1911d81a560f41c3d4e4a8
somgr <deniska9767@gmail.com>
2022-07-16 15:38:34 +0500

    upd

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ list_card_url = []
 headers = {"User-Agent":
            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}
 
-for count in range(1, 2):
+for count in range(1, 8):
     sleep(1)
     url = f'https://scrapingclub.com/exercise/list_basic/?page={count}'
 
