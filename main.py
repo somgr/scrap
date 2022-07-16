7f21ad3fe07afb06c8e5072eae0e9289033eef93
somgr <deniska9767@gmail.com>
2022-07-17 00:58:47 +0500

    write data to exel table

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from time import sleep
 
 headers = {"User-Agent":
-           "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}
+           "Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36"}
 
 def get_url():
     for count in range(1, 8):
@@ -20,15 +20,15 @@ def get_url():
             card_url = "https://scrapingclub.com" + i.find('a').get('href')
             yield card_url
 
-for card_url in get_url():
-    response = requests.get(card_url, headers=headers)
-    sleep(3)
-    soup = BeautifulSoup(response.text, 'lxml')
+def array():
+    for card_url in get_url():
+        response = requests.get(card_url, headers=headers)
+        sleep(3)
+        soup = BeautifulSoup(response.text, 'lxml')
 
-    data = soup.find('div', class_='card mt-4 my-4')
-    name = data.find('h3', class_='card-title').text
-    price = data.find('h4').text
-    text = data.find('p', class_='card-text').text
-    url_img = "https://scrapingclub.com" + data.find('img', class_='card-img-top img-fluid').get('src')
-
-    print(name + '\n' + price + '\n' + text + '\n' + url_img + '\n\n')
+        data = soup.find('div', class_='card mt-4 my-4')
+        name = data.find('h3', class_='card-title').text
+        price = data.find('h4').text
+        text = data.find('p', class_='card-text').text
+        url_img = "https://scrapingclub.com" + data.find('img', class_='card-img-top img-fluid').get('src')
+        yield name, price, text, url_img
